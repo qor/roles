acf9d6a398d49fc1f6d4373e8b1d3cca59c3b9bd
Jinzhu <wosmvp@gmail.com>
2017-08-02 14:06:49 +0800

    Export global role instance

--- global.go
+++ global.go
@@ -2,44 +2,45 @@ package roles
 
 import "net/http"
 
-var role = &Role{}
+// Global global role instance
+var Global = &Role{}
 
 // Register register role with conditions
 func Register(name string, fc func(request *http.Request, user interface{}) bool) {
-	role.Register(name, fc)
+	Global.Register(name, fc)
 }
 
 // Allow allows permission mode for roles
 func Allow(mode PermissionMode, roles ...string) *Permission {
-	return role.Allow(mode, roles...)
+	return Global.Allow(mode, roles...)
 }
 
 // Deny deny permission mode for roles
 func Deny(mode PermissionMode, roles ...string) *Permission {
-	return role.Deny(mode, roles...)
+	return Global.Deny(mode, roles...)
 }
 
 // Get role defination
 func Get(name string) (func(req *http.Request, user interface{}) bool, bool) {
-	return role.Get(name)
+	return Global.Get(name)
 }
 
 // Remove role definition from global role instance
 func Remove(name string) {
-	role.Remove(name)
+	Global.Remove(name)
 }
 
 // Reset role definitions from global role instance
 func Reset() {
-	role.Reset()
+	Global.Reset()
 }
 
 // MatchedRoles return defined roles from user
 func MatchedRoles(req *http.Request, user interface{}) []string {
-	return role.MatchedRoles(req, user)
+	return Global.MatchedRoles(req, user)
 }
 
 // HasRole check if current user has role
 func HasRole(req *http.Request, user interface{}, roles ...string) bool {
-	return role.HasRole(req, user)
+	return Global.HasRole(req, user)
 }
