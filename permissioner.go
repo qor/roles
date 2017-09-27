40e8ca5bae4b2333e95003e21796f8673afaad4c
Jinzhu <wosmvp@gmail.com>
2017-09-27 23:20:09 +0800

    Fix panic if Permissioner is nil

--- permissioner.go
+++ permissioner.go
@@ -7,7 +7,13 @@ type Permissioner interface {
 
 // ConcatPermissioner concat permissioner
 func ConcatPermissioner(ps ...Permissioner) Permissioner {
-	return permissioners(ps)
+	var newPS []Permissioner
+	for _, p := range ps {
+		if p != nil {
+			newPS = append(newPS, p)
+		}
+	}
+	return permissioners(newPS)
 }
 
 type permissioners []Permissioner
@@ -15,7 +21,7 @@ type permissioners []Permissioner
 // HasPermission check has permission for permissioners or not
 func (ps permissioners) HasPermission(mode PermissionMode, roles ...interface{}) bool {
 	for _, p := range ps {
-		if !p.HasPermission(mode, roles) {
+		if p != nil && !p.HasPermission(mode, roles) {
 			return false
 		}
 	}
